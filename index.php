1f6ce4b720f0512c81a29fedd8d069e00224aa8f
keerthanya <keerthanyashri@gmail.com>
2024-12-31 12:11:32 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram Login</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="login-container">
+        <h1>Instagram</h1>
+        <form>
+            <input type="text" placeholder="Phone number, username, or email" required>
+            <input type="password" placeholder="Password" required>
+            <button type="submit">Log in</button>
+        </form>
+        <div class="or">OR</div>
+        <a href="#" class="facebook-login">
+            <img src="https://img.icons8.com/color/16/000000/facebook.png" alt="Facebook Icon">
+            Log in with Facebook
+        </a>
+        <a href="#" class="forgot-password">Forgot password?</a>
+        <div class="signup">
+            Don't have an account? <a href="#">Sign up</a>
+        </div>
+    </div>
+</body>
+</html>
